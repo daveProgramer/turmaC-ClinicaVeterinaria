ca47f1d9cbf1150f90b2d392ecae241ae11d8905
jcasimiro <casimiro@ipt.pt>
2017-04-07 19:33:56 +0100

    criação das classes que representam as tabelas + classe que representa a Base de Dados

---
+++ ClinicaVeterinaria/ClinicaVeterinaria/Models/VetsDB.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace ClinicaVeterinaria.Models {
+   public class VetsDB : DbContext {
+
+      //***********************************************
+      //  informar a EF que as classes abeixo descritas
+      //  devem ser associadas a uma Base de Dados
+      //***********************************************
+      public virtual DbSet<Donos> Donos { get; set; }
+      public virtual DbSet<Animais> Animais { get; set; }
+
+
+      //***********************************************
+      //  onde criar e de que tipo será a Base de Dados
+      //***********************************************
+      public VetsDB() : base("localizacaoDaBD") { }
+
+   }
+}
